2a3174befb4ca85fd2e9f6ed152855871b83baa3
Ran <569155820@qq.com>
2022-08-22 09:43:53 +0800

    update bio: client connect to server

--- src/main/java/com/shuran/study/bio/Client.java
+++ src/main/java/com/shuran/study/bio/Client.java
@@ -7,27 +7,10 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 8888);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String input = null;
-            try {
-                input = reader.readLine();
-                sendToServer(socket,input);
-                if(input.equals("quit")){
-                    break;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    private static void sendToServer(Socket socket,String input) throws IOException {
-        if(!socket.isOutputShutdown()){
-            BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            writer.write(input+"\n");
-            writer.flush();
-        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line=reader.readLine();
+        System.out.println("from server: "+line);
+        reader.close();
+        socket.close();
     }
 }
--- src/main/java/com/shuran/study/bio/Server.java
+++ src/main/java/com/shuran/study/bio/Server.java
@@ -1,8 +1,6 @@
 package com.shuran.study.bio;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,28 +10,29 @@ import java.util.concurrent.Executors;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        ServerSocket ss=new ServerSocket();
-        ss.bind(new InetSocketAddress("127.0.0.1",8888));
-        while(true){
-            Socket socket=ss.accept();
-            new Thread(()->{
+        //创建server socket来监听客户端socket的连接请求
+        try (ServerSocket ss = new ServerSocket()) {
+            ss.bind(new InetSocketAddress("127.0.0.1", 8888));
+            //循环接收客户端的请求
+            while (true) {
+                //接收到请求后，服务端会对应产生一个socket
+                Socket socket = ss.accept();
                 handler(socket);
-            }).start();
+            }
         }
+
     }
 
     private static void handler(Socket socket){
         try {
-            BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String msg;
-            while((msg=reader.readLine())!=null){
-                System.out.println(msg);
-                if(msg.equals("quit")){
-                    break;
-                }
-            }
+            PrintStream printStream=new PrintStream(socket.getOutputStream());
+            printStream.println("SERVER HAS RECEIVED!");
+            socket.close();
+            printStream.close();
         } catch (IOException e){
             e.printStackTrace();
         }
     }
+
+
 }
